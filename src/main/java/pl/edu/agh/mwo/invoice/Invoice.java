a565925422685890488b2cc150bc5081349de29e
czudec <kaczudec@gmail.com>
2024-02-03 15:52:02 +0100

    implemented first for product tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,40 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+
+    private Collection<Product> products = new ArrayList<>();
+
+
 
     public void addProduct(Product product) {
-        // TODO: implement
+        products.add(product);
+
     }
 
     public void addProduct(Product product, Integer quantity) {
         // TODO: implement
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public BigDecimal getNetPrice() {
+        BigDecimal result =  BigDecimal.ZERO;
+        for (Product product : this.products) {
+            result = result.add(product.getPrice());
+
+        }
+        return  result;
     }
 
     public BigDecimal getTax() {
-        return null;
+        return BigDecimal.ZERO;
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGross() {
+        return BigDecimal.ZERO;
     }
 }
